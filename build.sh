fa4d9e4a6a8a926f7e4a244c46d2e411b9a2ce56
clint hidinger <chidinger@gmail.com>
2015-10-26 23:09:56 -0400

    More progress on embind.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-emcc --bind -o assimp.js -Iassimp/include/ -s WARN_ON_UNDEFINED_SYMBOLS=1 -s VERBOSE=1 -Dprivate=public assimpEmbind.cpp
+emcc --bind  -std=c++11 -o assimp.js -Iassimp/include/ -s WARN_ON_UNDEFINED_SYMBOLS=1 -s VERBOSE=1 -Dprivate=public assimpEmbind.cpp
 
 
 
