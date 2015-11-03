51d40f1a9160dd869c315b9ca5cdc3d3aef7eabc
clint hidinger <chidinger@gmail.com>
2015-11-02 20:50:43 -0500

    Got importer working.

--- ImporterEmbind.cpp
+++ ImporterEmbind.cpp
@@ -1,11 +1,27 @@
 
 #include <emscripten/bind.h>
-//#include "utilEmbind.h"
+#include "UtilEmbind.h"
 #include "assimp/Importer.hpp"
+#include "assimp/cimport.h"
 
 using namespace emscripten;
 using namespace Assimp;
 
+
+namespace aiPropertyStoreEmbind
+{
+	DefineGetterSetter(aiPropertyStore, char, sentinel, Sentinel)
+}
+
+namespace ImporterEmbind
+{
+	aiString getExtensionList(const Importer &importer)
+	{
+		aiString szOut;
+		importer.GetExtensionList(szOut);
+		return szOut;
+	}
+}
 
 EMSCRIPTEN_BINDINGS(ASSIMP)
 {
@@ -44,7 +60,7 @@ EMSCRIPTEN_BINDINGS(ASSIMP)
 	    .function("isExtensionSupported", select_overload<bool (const char*) const>(&Importer::IsExtensionSupported), allow_raw_pointers())
 	    .function("isExtensionSupported", select_overload<bool (const std::string&) const>(&Importer::IsExtensionSupported), allow_raw_pointers())
 	    .function("getExtensionList", select_overload<void (aiString&) const>(&Importer::GetExtensionList), allow_raw_pointers())
-	    .function("getExtensionList", &getExtensionList, allow_raw_pointers())
+	    .function("getExtensionList", &ImporterEmbind::getExtensionList, allow_raw_pointers())
 	    .function("getImporterCount", &Importer::GetImporterCount)
 	    .function("getImporterInfo", &Importer::GetImporterInfo, allow_raw_pointers())
 	    .function("getImporter", select_overload<BaseImporter *(size_t) const>(&Importer::GetImporter), allow_raw_pointers())
@@ -53,4 +69,46 @@ EMSCRIPTEN_BINDINGS(ASSIMP)
 	    .function("getMemoryRequirements", &Importer::GetMemoryRequirements, allow_raw_pointers())
 	    .function("setExtraVerbose", &Importer::SetExtraVerbose)
 	    ;
+
+	class_<aiLogStream>("aiLogStream")
+		;
+
+	class_<aiPropertyStore>("aiPropertyStore")
+		.function("getSentinel", &aiPropertyStoreEmbind::getSentinel)
+		.function("setSentinel", &aiPropertyStoreEmbind::setSentinel)
+		;
+
+	function("aiImportFile", &aiImportFile, allow_raw_pointers());
+	function("aiImportFileEx", &aiImportFileEx, allow_raw_pointers());
+	function("aiImportFileExWithProperties", &aiImportFileExWithProperties, allow_raw_pointers());
+	function("aiImportFileFromMemory", &aiImportFileFromMemory, allow_raw_pointers());
+	function("aiImportFileFromMemoryWithProperties", &aiImportFileFromMemoryWithProperties, allow_raw_pointers());
+	function("aiApplyPostProcessing", &aiApplyPostProcessing, allow_raw_pointers());
+	function("aiGetPredefinedLogStream", &aiGetPredefinedLogStream, allow_raw_pointers());
+	function("aiAttachLogStream", &aiAttachLogStream, allow_raw_pointers());
+	function("aiEnableVerboseLogging", &aiEnableVerboseLogging, allow_raw_pointers());
+	function("aiDetachLogStream", &aiDetachLogStream, allow_raw_pointers());
+	function("aiImpoaiDetachAllLogStreams", &aiDetachAllLogStreams, allow_raw_pointers());
+	function("aiReleaseImport", &aiReleaseImport, allow_raw_pointers());
+	function("aiGetErrorString", &aiGetErrorString, allow_raw_pointers());
+	function("aiIsExtensionSupported", &aiIsExtensionSupported, allow_raw_pointers());
+	function("aiGetMemoryRequirements", &aiGetMemoryRequirements, allow_raw_pointers());
+	function("aiCreatePropertyStore", &aiCreatePropertyStore, allow_raw_pointers());
+	function("aiReleasePropertyStore", &aiReleasePropertyStore, allow_raw_pointers());
+	function("aiSetImportPropertyInteger", &aiSetImportPropertyInteger, allow_raw_pointers());
+	function("aiSetImportPropertyFloat", &aiSetImportPropertyFloat, allow_raw_pointers());
+	function("aiSetImportPropertyString", &aiSetImportPropertyString, allow_raw_pointers());
+	function("aiSetImportPropertyMatrix", &aiSetImportPropertyMatrix, allow_raw_pointers());
+	function("aiCreateQuaternionFromMatrix", &aiCreateQuaternionFromMatrix, allow_raw_pointers());
+	function("aiDecomposeMatrix", &aiDecomposeMatrix, allow_raw_pointers());
+	function("aiTransposeMatrix4", &aiTransposeMatrix4, allow_raw_pointers());
+	function("aiTransposeMatrix3", &aiTransposeMatrix3, allow_raw_pointers());
+	function("aiTransformVecByMatrix3", &aiTransformVecByMatrix3, allow_raw_pointers());
+	function("aiTransformVecByMatrix4", &aiTransformVecByMatrix4, allow_raw_pointers());
+	function("aiMultiplyMatrix4", &aiMultiplyMatrix4, allow_raw_pointers());
+	function("aiMultiplyMatrix3", &aiMultiplyMatrix3, allow_raw_pointers());
+	function("aiIdentityMatrix3", &aiIdentityMatrix3, allow_raw_pointers());
+	function("aiIdentityMatrix4", &aiIdentityMatrix4, allow_raw_pointers());
+	function("aiGetImportFormatCount", &aiGetImportFormatCount, allow_raw_pointers());
+	function("aiGetImportFormatDescription", &aiGetImportFormatDescription, allow_raw_pointers());
 } // end EMSCRIPTEN_BINDINGS
