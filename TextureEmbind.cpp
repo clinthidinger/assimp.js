3f1e79b376d271b2b19f61a06fd9190d55196ebf
clint hidinger <chidinger@gmail.com>
2015-11-03 22:37:26 -0500

    Got texture working

--- TextureEmbind.cpp
+++ TextureEmbind.cpp
@@ -4,38 +4,71 @@
 #include "assimp/texture.h"
 
 using namespace emscripten;
-using namespace Assimp;
 
+namespace aiTexelEmbind
+{
+    DefineGetterSetter(aiTexel, unsigned char, b, B)
+    DefineGetterSetter(aiTexel, unsigned char, g, G)
+    DefineGetterSetter(aiTexel, unsigned char, r, R)
+    DefineGetterSetter(aiTexel, unsigned char, a, A)
+
+    void set(aiTexel &texel, unsigned char r, unsigned char g, unsigned char b, unsigned char a)
+    {
+        texel.r = r;
+        texel.g = g;
+        texel.b = b;
+        texel.a = a;
+    }
+}
+
+namespace aiTextureEmbind
+{
+    DefineGetterSetter(aiTexture, unsigned int, mWidth, Width)
+    DefineGetterSetter(aiTexture, unsigned int, mHeight, Height)
+    DefineGetter(aiTexture, const char *, achFormatHint, AchFormatHint)
+    DefineGetterSetter(aiTexture, aiTexel *, pcData, Data)
+
+
+    aiTexel *at(aiTexture &texture, unsigned int index)
+    {
+        return &texture.pcData[index];
+    }
+
+    aiTexel *at(aiTexture &texture,unsigned int rowIndex, unsigned int colIndex)
+    {
+        unsigned int index = (colIndex * texture.mWidth) + rowIndex;
+        return at(texture, index);
+    }
+}
 
 EMSCRIPTEN_BINDINGS(ASSIMP)
 {	
     class_<aiTexel>("aiTexel")
         .function("op_equal_to", &aiTexel::operator==)
         .function("op_not_equal_to", &aiTexel::operator!=)
-        .function("color_cast", &aiTexel::operator  aiColor4D)
-        .function("getB", &getB)
-        .function("setB", &setB)
-        .function("getG", &getG)
-        .function("setG", &setG)
-        .function("getR", &getR)
-        .function("setR", &setR)
-        .function("getA", &getA)
-        .function("setA", &setA)
-        .function("set", &set)
-        .function("at", &at)
+        .function("color4_cast", &aiTexel::operator  aiColor4D)
+        .function("getB", &aiTexelEmbind::getB)
+        .function("setB", &aiTexelEmbind::setB)
+        .function("getG", &aiTexelEmbind::getG)
+        .function("setG", &aiTexelEmbind::setG)
+        .function("getR", &aiTexelEmbind::getR)
+        .function("setR", &aiTexelEmbind::setR)
+        .function("getA", &aiTexelEmbind::getA)
+        .function("setA", &aiTexelEmbind::setA)
+        .function("set", &aiTexelEmbind::set)
         ;
         
     class_<aiTexture>("aiTexture")
         .constructor<aiTexture>()
-        .function("getWidth", &getWidth)
-        .function("setWidth", &setWidth)
-        .function("getHeight", &getHeight)
-        .function("setHeight", &setHeight)
-        .function("getAchFormatHint", &getAchFormatHint)
-        .function("setAchFormatHint", &setAchFormatHint)
-        .function("at", &at, allow_raw_pointers())
-        .function("getData", &getData, allow_raw_pointers())
-        .function("setData", &setData, allow_raw_pointers())
-        .function("checkFormat", &aiTexture::CheckFormat)
+        .function("getWidth", &aiTextureEmbind::getWidth)
+        .function("setWidth", &aiTextureEmbind::setWidth)
+        .function("getHeight", &aiTextureEmbind::getHeight)
+        .function("setHeight", &aiTextureEmbind::setHeight)
+        .function("getAchFormatHint", &aiTextureEmbind::getAchFormatHint, allow_raw_pointers())
+        .function("at", select_overload<aiTexel *(aiTexture &, unsigned int, unsigned int)>(&aiTextureEmbind::at), allow_raw_pointers())
+        .function("at", select_overload<aiTexel *(aiTexture &, unsigned int)>(&aiTextureEmbind::at), allow_raw_pointers())
+        .function("getData", &aiTextureEmbind::getData, allow_raw_pointers())
+        .function("setData", &aiTextureEmbind::setData, allow_raw_pointers())
+        .function("checkFormat", &aiTexture::CheckFormat, allow_raw_pointers())
         ;
 } // end EMSCRIPTEN_BINDINGS
