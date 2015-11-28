a08f76ad6d1dc25622d22e64b7eab1d627b50807
clint hidinger <chidinger@gmail.com>
2015-11-28 11:08:30 -0500

    Working on meshembind

--- include/UtilEmbind.h
+++ include/UtilEmbind.h
@@ -1,19 +1,41 @@
 #pragma once
+#include <vector>
+#include <algorithm>
 
 
 #define DefineGetter(ClassType, MemberType, MemberName, FuncName) 	MemberType get##FuncName(ClassType &object) { return object.MemberName; }
+#define DefineConstGetter(ClassType, MemberType, MemberName, FuncName) 	MemberType get##FuncName(const ClassType &object) { return object.MemberName; }
 #define DefineSetter(ClassType, MemberType, MemberName, FuncName)	void set##FuncName(ClassType &object, MemberType value) { object.MemberName = value; }
 
 #define DefineGetterSetter(ClassName, MemberType, MemberName, FuncName) \
 	DefineGetter(ClassName, MemberType, MemberName, FuncName) \
 	DefineSetter(ClassName, MemberType, MemberName, FuncName)
 
-class YoMama
-	{
-	public:
-		YoMama()
-		{
+#define DefineConstGetterSetter(ClassName, MemberType, MemberName, FuncName) \
+	DefineConstGetter(ClassName, MemberType, MemberName, FuncName) \
+	DefineSetter(ClassName, MemberType, MemberName, FuncName)
 
-		}
-		int y;
-	};
+
+#define DefineArrayGetter(ClassName, MemberType, MemberName, FuncName, Count) \
+	std::vector<MemberType> get##FuncName(const ClassName &object) \
+	{ \
+		std::vector<MemberType> array(Count); \
+		std::copy(object.MemberName, object.MemberName + Count, array.begin()); \
+		return array; \
+	}
+
+#define DefineArraySetter(ClassName, MemberType, MemberName, FuncName, Count) \
+	void set##FuncName(ClassName &object, const std::vector<MemberType> &array) \
+	{ \
+		if(object.MemberName != nullptr) \
+		{ \
+			delete[] object.MemberName; \
+		} \
+		object.MemberName = new float[Count]; \
+		std::copy(array.begin(), array.end(), object.MemberName); \
+	}
+
+#define DefineArrayGetterSetter(ClassName, MemberType, MemberName, FuncName, Count) \
+	DefineArrayGetter(ClassName, MemberType, MemberName, FuncName, Count) \
+	DefineArraySetter(ClassName, MemberType, MemberName, FuncName, Count)
+
--- src/MeshEmbind.cpp
+++ src/MeshEmbind.cpp
@@ -5,15 +5,82 @@
 
 using namespace emscripten;
 
+
+namespace aiFaceEmbind
+{
+	DefineGetterSetter(aiFace, unsigned int, mNumIndices, NumIndices)
+	DefineArrayGetterSetter(aiFace, unsigned int, mIndices, Indices, aiFace.mNumIndices)
+}
+
+namespace aiVertexWeightEmbind
+{
+	DefineGetterSetter(aiVertexWeight, unsigned int, VertexId, mVertexId)
+	DefineGetterSetter(aiVertexWeight, float, mWeight, Weight)
+}
+
+namespace aiBoneEmbind
+{
+	DefineGetterSetter(aiBone, aiString, mName, Name)
+	DefineGetterSetter(aiBone, unsigned int, mNumWeights, NumWeights)
+	DefineArrayGetterSetter(aiBone, aiVertexWeight, mWeights, Weights, aiBone.mNumWeights)
+	DefineConstGetterSetter(aiBone, aiMatrix4x4, mOffsetMatrix, OffsetMatrix)
+}
+
 namespace aiMeshEmbind
 {
-	
+	DefineGetterSetter(aiMesh, unsigned int, mPrimitiveTypes, PrimitiveTypes)
+	DefineGetterSetter(aiMesh, unsigned int, mNumVertices, NumVertices)
+	DefineGetterSetter(aiMesh, aiVector3D *, mVertices, Vertices)
+	DefineGetterSetter(aiMesh, unsigned int, mNumNormals, NumNormals)
+	DefineGetterSetter(aiMesh, aiVector3D *, mNormals, Normals)
+	DefineGetterSetter(aiMesh, unsigned int, mNumTangents, NumTangents)
+	DefineGetterSetter(aiMesh, aiVector3D *, mTangents, Tangents)
+	DefineGetterSetter(aiMesh, unsigned int, mNumBitangents, NumBitangents)
+	DefineGetterSetter(aiMesh, aiVector3D *, mBitangents, Bitangents)
+	DefineGetterSetter(aiMesh, unsigned int, mNumColors, NumColors)
+	DefineGetterSetter(aiMesh, aiVector4D *, mColors, Colors)
+	DefineGetterSetter(aiMesh, unsigned int, mTextureCoords, NumTextureCoords)
+	DefineGetterSetter(aiMesh, aiVector3D *, mTextureCoords, TextureCoords)
+}
 }
 
 
 
 EMSCRIPTEN_BINDINGS(assimp_mesh)
 {
+	class_<aiFace>("aiFace")
+		.constructor<>()
+		.constructor<const aiFace&>()
+		.function("getNumIndices", &aiFaceEmbind::getNumIndices)
+		.function("setNumIndices", &aiFaceEmbind::setNumIndices)
+		.function("getIndices", &aiFaceEmbind::getIndices)
+		.function("setIndices", &aiFaceEmbind::setIndices)
+		;
+
+	class_<aiVertexWeight>("aiVertexWeight")
+		.constructor<>()
+		.constructor<unsigned int, float>()
+		.function("getVertexId", &aiFaceEmbind::getVertexId)
+		.function("setVertexId", &aiFaceEmbind::setVertexId)
+		.function("getWeight", &aiFaceEmbind::getWeight)
+		.function("setWeight", &aiFaceEmbind::setWeight)
+		;
+
+	class_<aiBone>("aiBone")
+		.function("getName", &aiFaceEmbind::getName)
+		.function("setName", &aiFaceEmbind::setName)
+		.function("getNumWeights", &aiFaceEmbind::getNumWeights)
+		.function("setNumWeights", &aiFaceEmbind::setNumWeights)
+		;
+
+	enum_<aiPrimitiveType>("aiPrimitiveType")
+		.value("POINT", aiPrimitiveType_POINT)
+		.value("LINE", aiPrimitiveType_LINE)
+		.value("TRIANGLE", aiPrimitiveType_TRIANGLE)
+		.value("POLYGON", aiPrimitiveType_POLYGON)
+		;
+
+	
 	class_<aiMesh>("aiMesh")
 	    .property("mPrimitiveTypes", &aiMesh::mPrimitiveTypes)
 	    .property("mNumVertices", &aiMesh::mNumVertices)
