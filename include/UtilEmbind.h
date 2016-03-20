ac652358bbaae6537c0aa8bd77caea2558b7d8d5
clint hidinger <chidinger@gmail.com>
2016-03-20 13:40:28 -0400

    Got duck.dae loaded.  Need to test colors and bones now.

--- include/UtilEmbind.h
+++ include/UtilEmbind.h
@@ -40,35 +40,35 @@
 	DefineArraySetter(ClassName, MemberType, MemberName, FuncName, Count)
 
 
-#define DefineArraySetGetter(ClassName, MemberType, MemberName, FuncName, Count, Max) \
-	std::vector<MemberType> get##FuncName(const ClassName &object, unsigned int index) \
-	{ \
-		std::vector<MemberType> array(Count); \
-		if(index < Max) \
-		{ \
-			std::copy(object.MemberName[index], object.MemberName[index] + Count, array.begin()); \
-		} \
-		return array; \
-	}
+//#define DefineArraySetGetter(ClassName, MemberType, MemberName, FuncName, Count, Max) \
+//	std::vector<MemberType> get##FuncName(const ClassName &object, unsigned int index) \
+//	{ \
+//		std::vector<MemberType> array(Count); \
+//		if(index < Max) \
+//		{ \
+//			std::copy(object.MemberName[index], object.MemberName[index] + Count, array.begin()); \
+//		} \
+//		return array; \
+//	}
 
-#define DefineArraySetSetter(ClassName, MemberType, MemberName, FuncName, Count, Max) \
-	void set##FuncName(ClassName &object, const std::vector<MemberType> &array, unsigned int index) \
-	{ \
-		if(index >= Max) \
-		{ \
-			return; \
-		} \
-		if(object.MemberName[index] != nullptr) \
-		{ \
-			delete[] object.MemberName[index]; \
-		} \
-		object.MemberName[index] = new MemberType[Count]; \
-		std::copy(array.begin(), array.end(), object.MemberName[index]); \
-	}
+//#define DefineArraySetSetter(ClassName, MemberType, MemberName, FuncName, Count, Max) \
+//	void set##FuncName(ClassName &object, const std::vector<MemberType> &array, unsigned int index) \
+//	{ \
+//		if(index >= Max) \
+//		{ \
+//			return; \
+//		} \
+//		if(object.MemberName[index] != nullptr) \
+//		{ \
+//			delete[] object.MemberName[index]; \
+//		} \
+//		object.MemberName[index] = new MemberType[Count]; \
+//		std::copy(array.begin(), array.end(), object.MemberName[index]); \
+//	}
 
-#define DefineArraySetGetterSetter(ClassName, MemberType, MemberName, FuncName, Count, Max) \
-	DefineArraySetGetter(ClassName, MemberType, MemberName, FuncName, Count, Max) \
-	DefineArraySetSetter(ClassName, MemberType, MemberName, FuncName, Count, Max)
+//#define DefineArraySetGetterSetter(ClassName, MemberType, MemberName, FuncName, Count, Max) \
+//	DefineArraySetGetter(ClassName, MemberType, MemberName, FuncName, Count, Max) \
+//	DefineArraySetSetter(ClassName, MemberType, MemberName, FuncName, Count, Max)
 
 #define DefineArrayIndexGetter(ClassName, MemberType, MemberName, FuncName) \
 	MemberType get##FuncName(const ClassName &object, unsigned int index) \
--- src/MeshEmbind.cpp
+++ src/MeshEmbind.cpp
@@ -9,7 +9,6 @@ using namespace emscripten;
 namespace aiFaceEmbind
 {
 	DefineGetterSetter(aiFace, unsigned int, mNumIndices, NumIndices)
-	DefineArrayGetterSetter(aiFace, unsigned int, mIndices, Indices, object.mNumIndices)
 	DefineArrayIndexGetterSetter(aiFace, unsigned int, mIndices, Index)
 }
 
@@ -40,28 +39,12 @@ namespace aiMeshEmbind
 	DefineGetterSetter(aiMesh, unsigned int, mPrimitiveTypes, PrimitiveTypes)
 	DefineGetterSetter(aiMesh, unsigned int, mNumVertices, NumVertices)
 	DefineGetterSetter(aiMesh, unsigned int, mNumFaces, NumFaces)
-	//!!! Unbound typeDefineArrayGetterSetter(aiMesh, aiVector3D, mVertices, Vertices, object.mNumVertices)
 	DefineArrayIndexRefGetterSetter(aiMesh, aiVector3D, mVertices, Vertex)
-	DefineArrayGetterSetter(aiMesh, aiVector3D, mNormals, Normals, object.mNumVertices)
 	DefineArrayIndexRefGetterSetter(aiMesh, aiVector3D, mNormals, Normal)
-	DefineArrayGetterSetter(aiMesh, aiVector3D, mTangents, Tangents, object.mNumVertices)
 	DefineArrayIndexRefGetterSetter(aiMesh, aiVector3D, mTangents, Tangent)
-	DefineArrayGetterSetter(aiMesh, aiVector3D, mBitangents, Bitangents, object.mNumVertices)
 	DefineArrayIndexRefGetterSetter(aiMesh, aiVector3D, mBitangents, Bitangent)
-	DefineArraySetGetterSetter(aiMesh, aiColor4D, mColors, Colors, object.mNumVertices, AI_MAX_NUMBER_OF_COLOR_SETS)
-	unsigned int getNumUVComponents(const aiMesh &mesh, unsigned int setIndex)
-	{
-		return mesh.mNumUVComponents[setIndex];
-	}
-	void setNumUVComponents(aiMesh &mesh, unsigned int setIndex, unsigned int count)
-	{
-		mesh.mNumUVComponents[setIndex] = count;
-	}
-	DefineArraySetGetterSetter(aiMesh, aiVector3D, mTextureCoords, TextureCoords, object.mNumVertices, AI_MAX_NUMBER_OF_TEXTURECOORDS)
-	DefineArrayGetterSetter(aiMesh, aiFace, mFaces, Faces, object.mNumFaces)
 	DefineArrayIndexRefGetterSetter(aiMesh, aiFace, mFaces, Face)
 	DefineGetterSetter(aiMesh, unsigned int, mNumBones, NumBones)
-	DefineArrayGetterSetter(aiMesh, aiBone *, mBones, Bones, object.mNumBones)
 	DefineArrayIndexGetterSetter(aiMesh, aiBone *, mBones, Bone)
 	DefineGetterSetter(aiMesh, unsigned int, mMaterialIndex, MaterialIndex)
 	std::string getName(const aiMesh &mesh)
@@ -72,6 +55,30 @@ namespace aiMeshEmbind
 	{
 		mesh.mName = aiString(name);
 	}
+	unsigned int getNumUVComponents(const aiMesh &mesh, unsigned int setIndex)
+	{
+		return mesh.mNumUVComponents[setIndex];
+	}
+	void setNumUVComponents(aiMesh &mesh, unsigned int setIndex, unsigned int count)
+	{
+		mesh.mNumUVComponents[setIndex] = count;
+	}
+	const aiVector3D &getTextureCoord(const aiMesh &mesh, unsigned int uvSetIdx, unsigned int vertIdx)
+	{
+		return mesh.mTextureCoords[uvSetIdx][vertIdx];
+	}
+	void setTextureCoord(aiMesh &mesh, unsigned int uvSetIdx, unsigned int vertIdx, const aiVector3D &value)
+	{
+		mesh.mTextureCoords[uvSetIdx][vertIdx] = value;
+	}
+	const aiColor4D &getColor(const aiMesh &mesh, unsigned int colorSetIdx, unsigned int vertIdx)
+	{
+		return mesh.mColors[colorSetIdx][vertIdx];
+	}
+	void setColor(aiMesh &mesh, unsigned int colorSetIdx, unsigned int vertIdx, const aiColor4D &value)
+	{
+		mesh.mColors[colorSetIdx][vertIdx] = value;
+	}
 }
 
 EMSCRIPTEN_BINDINGS(assimp_mesh)
@@ -81,8 +88,8 @@ EMSCRIPTEN_BINDINGS(assimp_mesh)
 		.constructor<const aiFace&>()
 		.function("getNumIndices", &aiFaceEmbind::getNumIndices)
 		.function("setNumIndices", &aiFaceEmbind::setNumIndices)
-		.function("getIndices", &aiFaceEmbind::getIndices)
-		.function("setIndices", &aiFaceEmbind::setIndices)
+		.function("getIndex", &aiFaceEmbind::getIndex)
+		.function("setIndex", &aiFaceEmbind::setIndex)
 		.function("op_equals", &aiFace::operator=)
 		.function("op_equal_to", &aiFace::operator==)
 		.function("op_not_equal_to", &aiFace::operator!=)
@@ -121,34 +128,26 @@ EMSCRIPTEN_BINDINGS(assimp_mesh)
 	    .function("setNumVertices", &aiMeshEmbind::setNumVertices)
 	    .function("getNumFaces", &aiMeshEmbind::getNumFaces)
 	    .function("setNumFaces", &aiMeshEmbind::setNumFaces)
-	    .function("getVertices", &aiMeshEmbind::getVertices)
-	    .function("setVertices", &aiMeshEmbind::setVertices)
 	    .function("getVertex", &aiMeshEmbind::getVertex)
 	    .function("setVertex", &aiMeshEmbind::setVertex)
-	    .function("getNormals", &aiMeshEmbind::getNormals)
-	    .function("setNormals", &aiMeshEmbind::setNormals)
 	    .function("getNormal", &aiMeshEmbind::getNormal)
 	    .function("setNormal", &aiMeshEmbind::setNormal)
-	    .function("getTangents", &aiMeshEmbind::getTangents)
-	    .function("setTangents", &aiMeshEmbind::setTangents)
 	    .function("getTangent", &aiMeshEmbind::getTangent)
 	    .function("setTangent", &aiMeshEmbind::setTangent)
-	    .function("getBitangents", &aiMeshEmbind::getBitangents)
-	    .function("setBitangents", &aiMeshEmbind::setBitangents)
-	     .function("getBitangent", &aiMeshEmbind::getBitangent)
+	    .function("getBitangent", &aiMeshEmbind::getBitangent)
 	    .function("setBitangent", &aiMeshEmbind::setBitangent)
-	    .function("getColors", &aiMeshEmbind::getColors)
-	    .function("setColors", &aiMeshEmbind::setColors)
-	    .function("getTextureCoords", &aiMeshEmbind::getTextureCoords)
-	    .function("setTextureCoords", &aiMeshEmbind::setTextureCoords)
+	    .function("getColor", &aiMeshEmbind::getColor)
+	    .function("setColor", &aiMeshEmbind::setColor)
+	    .function("getTextureCoord", &aiMeshEmbind::getTextureCoord)
+	    .function("setTextureCoord", &aiMeshEmbind::setTextureCoord)
 	    .function("getNumUVComponents", &aiMeshEmbind::getNumUVComponents)
 	    .function("setNumUVComponents", &aiMeshEmbind::setNumUVComponents)
-	    .function("getFaces", &aiMeshEmbind::getFaces)
-	    .function("setFaces", &aiMeshEmbind::setFaces)
+	    .function("getFace", &aiMeshEmbind::getFace)
+	    .function("setFace", &aiMeshEmbind::setFace)
 	    .function("getNumBones", &aiMeshEmbind::getNumBones)
 	    .function("setNumBones", &aiMeshEmbind::setNumBones)
-	    .function("getBones", &aiMeshEmbind::getBones)
-	    .function("setBones", &aiMeshEmbind::setBones)
+	    //!!!.function("getBone", &aiMeshEmbind::getBone)
+	    //!!!.function("setBone", &aiMeshEmbind::setBone)
 	    .function("getMaterialIndex", &aiMeshEmbind::getMaterialIndex)
 	    .function("setMaterialIndex", &aiMeshEmbind::setMaterialIndex)
 	    .function("getName", &aiMeshEmbind::getName)
